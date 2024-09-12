b5cc06524118a43c07935a2e98187c63afd13932
Akash <akashspalloor@gmail.com>
2024-09-13 00:47:21 +0530

    AXIO calculator

---
+++ script.js
@@ -0,0 +1,11 @@
+let screen = document.getElementById('screen');
+function btnclick(value){
+    screen.value+=value;
+}
+function clearscreen(){
+    screen.value="";
+}
+function findresult(){
+    var result = eval(screen.value);
+    screen.value = result
+}
